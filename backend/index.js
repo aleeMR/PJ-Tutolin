087d9f5b15f625a8d0fd2d21cd6533e8ba7a3d53
aleeMR <alee_1225@hotmail.com>
2022-02-26 23:19:38 -0500

    HU-TL-013: Creando rutas y entidad de servicios

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,33 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
 
+// Environment Variables (Variables de entorno)
+require('dotenv').config();
+
 // Settings (Configuraciones)
+// ---------------------------------------------------------------
 app.set('port', process.env.PORT || 3000);
 
 // Middlewares (Programas intermedios)
+// ---------------------------------------------------------------
 // Para ver las peticiones al servidor con un formateado de texto
 app.use(morgan('dev'));
 // Para verificar si los datos provienen en formato json
 app.use(express.json());
 
 // Routes (Rutas)
-
+// ---------------------------------------------------------------
+app.use('/api/services', require('./routes/service.routes'));
 
 // Static files (Archivos estáticos)
-
+// ---------------------------------------------------------------
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Starting the server (Iniciando el servidor)
+// ---------------------------------------------------------------
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
 });
