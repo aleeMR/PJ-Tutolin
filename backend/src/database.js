39d1aa3c107cf793f9dfea715851ff91169b587f
aleeMR <alee_1225@hotmail.com>
2022-03-14 21:56:30 -0500

    HU-TL-013: Creando método para listar servicios

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,3 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI);
+// Environment Variables (Variables de entorno)
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(db => console.log('DB is connected on host ' + db.connection.host))
+    .catch(er => console.error('DB no connected ' + er));
+
+module.exports = mongoose;
--- backend/src/routes/service.routes.js
+++ backend/src/routes/service.routes.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        status: 'API works'
-    });
+const Service = require('../models/service');
+
+// Método para traer todos los servicios
+router.get('/', async (req, res) => {
+    const services = await Service.find();
+    res.json(services);
+});
+
+// Método para crear servicios
+router.post('/', async (req, res) => {
+    console.log(req.body);
 });
 
 module.exports = router;
