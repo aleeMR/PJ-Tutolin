7aa82c551f2ea3a98f74d19e2d26b9a5222e6d63
aleeMR <alee_1225@hotmail.com>
2022-03-20 11:32:41 -0500

    HU-TL-017: Método para crear un servicio modificado

--- backend/src/controllers/service.controller.js
+++ backend/src/controllers/service.controller.js
@@ -11,43 +11,29 @@ const listServices = async (req, res) => {
     });
 };
 
-// Método para registrar un usuario
-const signup = async (req, res) => {
+// Método para crear un servicio
+const createService = async (req, res) => {
     const {
-        name,
-        surname,
-        email,
-        password
+        title,
+        description,
+        price
     } = req.body;
 
-    // Buscamos el correo electrónico del usuario en la BD
-    const user = await User.findOne({ email });
-
-    // Si el usuario ya está registrado
-    if (user)
-        return res.status(400).json({
-            msg: "Correo electrónico ya registrado."
-        });
-    // Si el usuario no está registrado
-    const newUser = new User({
-        name,
-        surname,
-        email,
-        password: await new User().encryptPassword(password)
+    const service = new Service({
+        title,
+        description,
+        price
     });
-    // Guardamos el usuario en la BD
-    const userSaved = await newUser.save();
-
-    const token = jwt.sign({id: userSaved._id}, process.env.JWT_SECRET);
+    // Guardamos el servicio en la BD
+    const serviceSaved = await service.save();
 
     res.status(200).json({
-        userSaved,
-        token,
-        msg: "Usuario registrado exitosamente."
+        serviceSaved,
+        msg: "Servicio creado exitosamente."
     });
 };
 
 module.exports = {
     listServices,
-    signup
+    createService
 }
--- backend/src/routes/service.routes.js
+++ backend/src/routes/service.routes.js
@@ -1,27 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const { verifyToken } = require('../middlewares/verifyToken');
 const ServiceCtrl = require('../controllers/service.controller');
 
 // Método para listar todos los servicios
 router.get('/', ServiceCtrl.listServices);
 
-// Método para crear servicios
-router.post('/', async (req, res) => {
-    const {
-        title,
-        description,
-        price
-    } = req.body;
-
-    const service = new Service({
-        title,
-        description,
-        price
-    });
-    await service.save();
-
-    res.json({status: 'Servicio creado exitosamente.'});
-});
+// Método para crear un servicio
+router.post('/', verifyToken, ServiceCtrl.createService);
 
 module.exports = router;
